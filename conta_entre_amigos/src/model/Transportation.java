d6e7f711da7c741ea459fb7f4519fc0789e4af30
Bruno Ribeiro <bbrunoo@icloud.com>
2022-08-11 17:02:32 -0300

    reorganizing branch maste

---
+++ conta_entre_amigos/src/model/Transportation.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Transportation extends Arrangement {
+	
+	private String transportMode;
+    private double transportCost;
+
+    
+    public String getTransportMode() {
+        return transportMode;
+    }
+    public void setTransportMode(String transportMode) {
+        this.transportMode = transportMode;
+    }
+    public double getTransportCost() {
+        return transportCost;
+    }
+    public void setTransportCost(double transportCost) {
+        this.transportCost = transportCost;
+    }
+    
+    
+    public double sumPrices(){
+        return 2.3;
+    }
+    
+    public void calculateCarTravelPrice(double distance, double vehicle, double gasPrice){
+        
+    }
+    
+    @Override
+    public void toPostpone() {
+        
+    }
+
+    @Override
+    public void antecipate() {
+      
+    }
+
+    @Override
+    public boolean cancel() {
+        return true;
+    }
+}
---
+++ conta_entre_amigos/src/model/User.java
@@ -0,0 +1,64 @@
+package model;
+
+public class User {
+	
+	private String name;
+    private String birthday;
+    private String [] friends;
+    private String [] yourGroups;
+    private double balance;
+    
+    //get and set;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String[] getFriends() {
+        return friends;
+    }
+    public void setFriends(String friends[]) {
+        this.friends = friends;
+    }
+
+    public String[] getYourGroups() {
+        return yourGroups;
+    }
+    public void setYourGroups(String yourGroups[]) {
+        this.yourGroups = yourGroups;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public boolean deposity (User friend, double amount){
+        return true;
+    }
+    
+    public void addFriend(User friend){
+        
+    } 
+    
+    public void removeFriend(User friend){
+        
+    }
+    
+    public String friendList(){
+        return "oi";
+    }
+
+}
