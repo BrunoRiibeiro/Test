efa92da633161cd251de9fa87c269be2b0db490a
Igor <igor.penharol@gmail.com>
2022-08-18 14:30:15 -0300

    implementing a "database"

---
+++ goDutch_with_friends/src/model/database/DatabaseProvider.java
@@ -0,0 +1,26 @@
+package model.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Group;
+import model.User;
+
+public class DatabaseProvider {
+	private static final List<User> USERS = new ArrayList<>();
+	private static final List<Group> GROUPS = new ArrayList<>();
+	
+	
+	private DatabaseProvider() {
+		
+	}
+
+	public static List<User> getUsers() {
+		return USERS;
+	}
+
+	public static List<Group> getGroups() {
+		return GROUPS;
+	}
+
+}
---
+++ goDutch_with_friends/src/orchestrates/Main.java
@@ -0,0 +1,7 @@
+package orchestrates;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
