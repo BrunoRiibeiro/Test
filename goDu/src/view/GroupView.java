bc144b0b5056e1b07317596ca06e31d101b7b6f9
Bruno Ribeiro <bbrunoo@icloud.com>
2022-08-26 19:56:05 -0300

    deleting some old imports

--- goDu/src/view/GroupView.java
+++ goDu/src/view/GroupView.java
@@ -1,18 +1,10 @@
 package view;
 
-import java.awt.Color;
-import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
-import javax.swing.border.LineBorder;
 
 import controller.GroupController;
 
