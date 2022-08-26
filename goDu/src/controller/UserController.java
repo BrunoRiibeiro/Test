bb5d268f3225a29eafbf0445b5805c026abef994
Bruno Ribeiro <bbrunoo@icloud.com>
2022-08-26 20:05:35 -0300

    setting User buttons

--- goDu/src/controller/UserController.java
+++ goDu/src/controller/UserController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import javax.swing.DefaultListModel;
+
+import model.User;
+import model.database.DatabaseProvider;
+import view.Home;
+import view.RegisterGroup;
 import view.UserView;
 
 public class UserController {
 	private final UserView view;
+	private User pickedUser;
 
 	public UserController(UserView view) {
 		super();
@@ -11,7 +18,40 @@ public class UserController {
 	}
 
 	public void sendAction(Object source) {
-		// TODO Auto-generated method stub
+		if (source == view.getButtonBack()) {
+			new Home();
+		} else if (source == view.getButtonEdit()) {
+			String pickedName = view.getFieldGroups().getSelectedValue();
 
+			if (pickedName != null) {
+				pickedUser = recoverPickedUser(pickedName);
+				// new RegisterUser();
+				// MainFrameControl.mostrarCadastraPapel();
+			}
+		} else if (source == view.getButtonNewGroup()) {
+			new RegisterGroup();
+		}
+	}
+	public User getPickedUser() {
+		return pickedUser;
+	}
+	
+	public User recoverPickedUser(String name) {
+		for (User currentUser : DatabaseProvider.getUsers()) {
+			if (name.equals(currentUser.getName())) {
+				return currentUser;
+			}
+		}
+		return null;
+	}
+
+	public DefaultListModel<String> updateList() {
+		DefaultListModel<String> names = new DefaultListModel<>();
+
+		for (User currentUser : DatabaseProvider.getUsers()) {
+			names.addElement(currentUser.getName());
+		}
+
+		return names;
 	}
 }
