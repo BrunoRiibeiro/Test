40cd59f6be26915399ba55a23bdd18ce6ea75092
Bruno Ribeiro <bbrunoo@icloud.com>
2022-09-02 11:42:19 -0300

    first javaDoc HomeController and EditGroupController

--- goDu/src/controller/EditGroupController.java
+++ goDu/src/controller/EditGroupController.java
@@ -7,15 +7,41 @@ import view.RegisterRestaurant;
 import view.RegisterTransportation;
 import view.ShowGroup;
 
+/**
+ * Classe controller da view.EditGroup, tem como função gerenciar e linkar os botões clicados com suas respectivas páginas.
+ * 
+ * @see view.EditGroup
+ * @see view.RegisterAccommodation
+ * @see view.RegisterEditDataGroup
+ * @see view.RegisterRestaurant
+ * @see view.RegisterTransportation
+ * @see view.ShowGroup
+ * 
+ * @author Bruno Ribeiro
+ * @author Igor Penha
+ */
 public class EditGroupController {
 
 	private final EditGroup view;
-
+	
+	/**
+	 * Construtor recebe a view a qual irá gerenciar.
+	 * 
+	 * @param view
+	 */
 	public EditGroupController(EditGroup view) {
 		super();
 		this.view = view;
 	}
-
+	
+	/**
+	 * Método recebe um Object capiturado pelo método herdado da interface ActionListner nas classes view. Redireciona a página 
+	 * de acordo com o botão clicado.
+	 * 
+	 * 
+	 * @see java.util.Objects
+	 * @param source
+	 */
 	public void sendAction(Object source) {
 		if (source == view.getButton1()) {
 			new RegisterAccommodation();
